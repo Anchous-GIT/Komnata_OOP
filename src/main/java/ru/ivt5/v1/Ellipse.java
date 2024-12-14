7f2398825f1772da57e78be7bb998c58a6a6c911
Anchous-GIT <mutver695@gmail.com>
2024-12-14 21:23:25 +0600

    Initial commit

--- src/main/java/ru/ivt5/v1/Ellipse.java
+++ src/main/java/ru/ivt5/v1/Ellipse.java
@@ -37,9 +37,70 @@ public class Ellipse {
         return xAxis;
     }
 
+    // 7.
     public int getYAxis() {
         return yAxis;
     }
+
+    public void setXAxis(int xAxis){
+        this.xAxis = xAxis;
+    }
+
+    public void setYAxis(int yAxis){
+        this.yAxis = yAxis;
+    }
+
+    public void setCenter(Point center){
+        this.center = center;
+    }
+
+    public void moveTo(int x, int y){
+        this.center.setX(x);
+        this.center.setY(y);
+    }
+
+    public void moveTo(Point point){
+        this.center = point;
+    }
+
+    public void moveRel(int dx, int dy){
+        this.center = new Point(this.center.getX()+dx, this.center.getY()+dy);
+
+    }
+
+    public void resize(double ratio){
+        this.xAxis = (int) (this.xAxis * ratio);
+        this.yAxis = (int) (this.yAxis  * ratio);
+    }
+
+    public void stretch(double xRatio, double yRatio){
+        this.xAxis = (int) (xAxis * xRatio);
+        this.yAxis = (int) (yAxis * yRatio);
+    }
+
+    public double getArea(){
+        return Math.PI * xAxis*yAxis;
+    }
+
+    public double getPerimeter(){
+        return 2 * Math.PI * Math.sqrt((Math.pow(this.xAxis, 2) + Math.pow(this.yAxis, 2)) / 8);
+    }
+
+    public boolean isInside(int x, int y) {
+        double dx = (x - this.center.getX()) / (double) this.xAxis;
+        double dy = (y - this.center.getY()) / (double) this.yAxis;
+        return dx * dx + dy * dy <= 1;
+    }
+
+    public boolean isInside(Point point) {
+        return isInside(point.getX(), point.getY());
+    }
+
+
+
+
+
+
 
 
 }
